7a5c91bdd94a45d13da8662d3fa118a942164c7e
Nicolas Coutureau <nicolas.coutureau@gmail.com>
2023-04-06 23:06:54 +0200

    wip

--- src/api/routes/status.js
+++ src/api/routes/status.js
@@ -1,3 +1,7 @@
 module.exports = (router) => {
     router.get('/status', (req, res) => res.json('ok'));
+
+    router.get('/status/:id', (req, res) => {    
+        res.json(`ok ${req.params.id}`);
+    });
 };
